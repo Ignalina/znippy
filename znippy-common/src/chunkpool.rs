f6d377d4979aa1ff3cb6a439f1fe988a430e91de
Rickard Lundin <rickard@ignalina.dk>
2025-07-02 23:15:55 +0200

    fixed chunkpool

--- znippy-common/src/chunkpool.rs
+++ znippy-common/src/chunkpool.rs
@@ -1,35 +1,31 @@
-// znippy-common/src/chunkpool.rs
-
 use crate::common_config::CONFIG;
-use crate::int_ring::RingBuffer;
+use crate::int_ring::{RingBuffer, ChunkQueue};
 use std::sync::Arc;
 
 /// Enkeltrådad pool av återanvändbara chunk-buffertar.
 pub struct ChunkPool {
-    buffers: Vec<Arc<Box<[u8; CHUNK_SIZE]>>>,
+    buffers: Vec<Arc<Box<[u8]>>>,
     ring: RingBuffer,
 }
 
-const CHUNK_SIZE:usize  = CONFIG.file_split_block_size as usize;
-
 impl ChunkPool {
     pub fn new() -> Self {
+        let chunk_size = CONFIG.file_split_block_size_usize();
         let num_chunks = CONFIG.max_chunks;
-        let mut buffers = Vec::with_capacity(num_chunks);
-        let mut ring = RingBuffer::new(num_chunks as u32);
-
+        let mut buffers = Vec::with_capacity(num_chunks as usize);
+        let mut ring = RingBuffer::new(num_chunks as usize);
         for i in 0..num_chunks {
-            let buf = Box::new([0u8; CHUNK_SIZE]);
+            let buf = vec![0u8; chunk_size].into_boxed_slice();
             buffers.push(Arc::new(buf));
-            ring.push(i as u32);
+            ring.push(i);
         }
 
         Self { buffers, ring }
     }
 
-    /// Hämtar nästa lediga chunk-index.
+    /// Hämtar nästa lediga chunk-index från ringen.
     pub fn get_index(&mut self) -> u32 {
-        self.ring.pop()
+        self.ring.pop().expect("RingBuffer underrun: inga lediga chunk-index kvar")
     }
 
     /// Returnerar ett chunk-index till poolen.
@@ -37,8 +33,8 @@ impl ChunkPool {
         self.ring.push(index);
     }
 
-    /// Hämtar Arc till buffer givet ett index.
-    pub fn get_buffer(&self, index: u32) -> Arc<Box<[u8; CHUNK_SIZE]>> {
+    /// Hämtar Arc till en buffer givet ett index.
+    pub fn get_buffer(&self, index: u32) -> Arc<Box<[u8]>> {
         Arc::clone(&self.buffers[index as usize])
     }
 }
